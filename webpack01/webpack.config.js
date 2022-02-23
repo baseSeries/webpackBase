052bb3064546d123d216acba778ec07d272e87f7
dang <862294826@qq.com>
2022-02-23 10:53:08 +0800

    clean

--- webpack01/webpack.config.js
+++ webpack01/webpack.config.js
@@ -1,10 +1,20 @@
 const path = require('path')
-
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 module.exports = {
-    entry: './src/index.js',
+    entry: './src/main.js',
     output: {
         path: path.join(__dirname, 'dist'),
         filename: 'bundle.js',
+    },
+    module: {
 
-    }
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            template: path.join(__dirname, './src/assets/index.html'),
+        })
+    ],
+
 }
